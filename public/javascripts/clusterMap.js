60f3dc6575149ba7c9b8309003f283ac1c639d58
sridharp45 <psridhar@iitg.ac.in>
2022-02-14 18:26:56 +0530

    some changes

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/dark-v10',
-    center: [-103.5917, 40.6699],
+    center: [20.5937, 78.9629],
     zoom: 3
 });
 

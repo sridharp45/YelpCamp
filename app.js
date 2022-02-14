2521a3ba566ea1e2d3102b4c60297912b55be788
sridharp45 <psridhar@iitg.ac.in>
2022-02-14 17:20:15 +0530

    err

--- app.js
+++ app.js
@@ -95,5 +95,5 @@ app.use((err, req, res, next) => {
 })
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log('Serving on port 5000')
+    console.log(`Serving on port ${port}`)
 })

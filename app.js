b8b1762ac8a027d835e33b191c4ab982110d878f
sridharp45 <psridhar@iitg.ac.in>
2022-02-14 16:32:44 +0530

    just trying to commit something

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const MongoStore = require('connect-mongo');
 const app = express();
 
 const dburl = process.env.DB_URL || 'mongodb://localhost:27017/camp';
-// 'mongodb://localhost:27017/camp'
+const t = 'mongodb://localhost:27017/camp';
 mongoose.connect(dburl);
 
 const db = mongoose.connection;
